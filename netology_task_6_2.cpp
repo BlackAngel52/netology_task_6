e3abd4969d45514f260c936633807e1e45f583a2
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 19:01:57 +0300

    Add files via upload

---
+++ netology_task_6_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int valueToPowerX(int value, int power) {
+	int result{ 1 };
+
+	for (int i = 0; i < power; i++) 
+	{
+		result *= value;
+	}
+
+	return result;
+}
+
+int main() {
+
+	int value{ 5 };
+	int power{ 2 };
+
+	std::cout << value << " to power " << power << " = " << valueToPowerX(value, power) << std::endl;
+
+	value = 3;
+	power = 3;
+
+	std::cout << value << " to power " << power << " = " << valueToPowerX(value, power) << std::endl;
+
+	value = 4;
+	power = 4;
+
+	std::cout << value << " to power " << power << " = " << valueToPowerX(value, power) << std::endl;
+
+
+
+	return 0;
+}
